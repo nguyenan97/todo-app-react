4fb65016325d78b330cebd5001edd203557b9c28
nguyenbinhanit <nguyenbinhanit@gmail.com>
2019-12-27 15:49:27 +0700

    Updates

---
+++ precache-manifest.b25da203341c6b770f1f700f4edf4011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a99892cb22eb452fc9b158b0e1218b",
+    "url": "/nguyenan97/todo-app-react/index.html"
+  },
+  {
+    "revision": "a831fd75266393a2c0a2",
+    "url": "/nguyenan97/todo-app-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3f0ec15ec5c3a1dda583",
+    "url": "/nguyenan97/todo-app-react/static/js/2.7211b22f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nguyenan97/todo-app-react/static/js/2.7211b22f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a831fd75266393a2c0a2",
+    "url": "/nguyenan97/todo-app-react/static/js/main.fd041eb6.chunk.js"
+  },
+  {
+    "revision": "ccd1aeea01b737c92931",
+    "url": "/nguyenan97/todo-app-react/static/js/runtime-main.171be4d3.js"
+  }
+]);
